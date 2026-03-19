992ebb3bb6fda47daa5fddce9302fddd36df0155
pavlyxa7191-collab <pavlyxa7191@yandex.ru>
2026-03-19 17:15:56 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { registerOAuthRoutes } from "../server/_core/oauth.ts";
-import { registerChatRoutes } from "../server/_core/chat.ts";
-import { registerAmoCrmOAuthRoutes } from "../server/amocrmOAuth.ts";
-import { appRouter } from "../server/routers.ts";
-import { createContext } from "../server/_core/context.ts";
+import { registerOAuthRoutes } from "../server/_core/oauth.js";
+import { registerChatRoutes } from "../server/_core/chat.js";
+import { registerAmoCrmOAuthRoutes } from "../server/amocrmOAuth.js";
+import { appRouter } from "../server/routers.js";
+import { createContext } from "../server/_core/context.js";
 
 const app = express();
 
